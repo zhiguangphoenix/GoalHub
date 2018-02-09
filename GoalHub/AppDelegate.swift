6a0be40abc55b872368f296e5d9afd24029d4e37
g1eny0ung <g1enyy0ung@gmail.com>
2018-02-09 18:26:52 +0800

    Initial Commit

---
+++ GoalHub/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  GoalHub
+//
+//  Created by 杨钺 on 2018/2/9.
+//  Copyright © 2018年 ilime. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
